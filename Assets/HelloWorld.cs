374a265b27437c9ecd66f4fa80967e952cb22ac5
ASHISH AAPAN <ashishaapan@gmail.com>
2018-04-17 21:57:08 +0530

    init commit

---
+++ Assets/HelloWorld.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour {
+
+
+
+	private Script2 sc;
+	// Use this for initialization
+	void Start () {
+		sc = GetComponent<Script2>();
+		sc.helloFromScript2();
+
+		var gr = GameObject.Find("ground");
+
+		if(gr!=null){
+			Debug.Log("found game object");
+		}
+		else{
+			Debug.Log("not found");
+		}
+
+		Transform cubeTrans = transform.Find("Cube");
+		if(cubeTrans!=null){
+			Debug.Log("cube is not null " +cubeTrans.gameObject.name);
+
+		}
+
+	
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Debug.Log(this.gameObject.name);
+
+
+
+		if(Input.GetKey(KeyCode.UpArrow)){
+			transform.position += new Vector3(0,1,0);
+		}
+
+
+		if(Input.GetKey(KeyCode.DownArrow)){
+			transform.position += new Vector3(0,-1,0);
+		}
+
+
+		if(Input.GetKey(KeyCode.RightArrow)){
+			transform.position += new Vector3(1,0,0);
+		}
+
+
+		if(Input.GetKey(KeyCode.LeftArrow)){
+			transform.position += new Vector3(-1,0,0);
+		}
+
+
+
+
+		
+	}
+}
---
+++ Assets/Script2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script2 : MonoBehaviour {
+
+
+	public void helloFromScript2(){
+		Debug.Log("hello");
+	}
+	private HelloWorld hello;
+	// Use this for initialization
+	void Start () {
+		hello = GetComponent<HelloWorld>();
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
